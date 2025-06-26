8c4e24c615b41e347decc38c5b62e5268cbfec4b
Nur-Sidik <nursidik1500@gmail.com>
2025-06-26 11:08:41 +0700

    menambahkan menu interaktif untuk memasukkan nilai dan melakukan traversal pohon

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -133,5 +133,51 @@ class BinaryTree
 int main()
 {
     BinaryTree x;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Masukkan Sebuah Nilai" << endl;
+        cout << "2. Lakukan Traversal Inorder" << endl;
+        cout << "3. Lakukan Traversal Preorder" << endl;
+        cout << "4. Lakukan Traversal Postorder" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "\nMasukan Pilihan Anda (1-5) : ";
 
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            x.insert();
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5':
+            return 0;
+        default:
+        {
+            cout << "Pilihan tidak valid" << endl;
+            break;
+        }
+        }
+    }
 };
